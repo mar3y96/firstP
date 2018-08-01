978e366b3be09438a409632935585fa9fe0f4b7e
Mar3y96 <mohammedmaray9674@gmail.com>
2018-08-02 00:58:29 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel from git
+                    Laravel from git ..
                 </div>
 
                 <div class="links">
